8954660e8e6dcfbffaa3810cec2c66c2f1b0eb9b
jeherve <jeherve@users.noreply.github.com>
2023-01-16 10:38:54 +0000

    Introduce Jetpack Migration plugin (#28209)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3929389724

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'eec0af4106d566e9aa1c');
